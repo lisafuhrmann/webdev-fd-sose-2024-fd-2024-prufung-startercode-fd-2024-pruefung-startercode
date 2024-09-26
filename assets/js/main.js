770f24abefa1b4c51792a89358a5052752c336c4
lisafuhrmann <lisa_marie.fuhrmann@smail.th-koeln.de>
2024-09-26 13:12:20 +0200

    F6: Create basic version of page

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,31 +20,38 @@ async function loadFinishedWorks() {
     const worksList = document.querySelector("[data-js-finished-works]");
     worksList.classList.add("works-grid");
 
-    works.slice(-5).forEach((work) => {
-      const workItem = document.createElement("li");
-      workItem.classList.add("work-item");
+    works
+      .slice(-5)
+      .reverse()
+      .forEach((work, index) => {
+        const workItem = document.createElement("li");
+        workItem.classList.add("work-item");
 
-      const date = new Date(work.date);
-      const formattedDate = date.toLocaleDateString("de-DE", {
-        month: "long",
-        year: "numeric",
-      });
+        const link = index === 3 ? "works/n-pola-can-i-cai.html" : work.url;
 
-      workItem.innerHTML = `
-      <div class="work-content">
-          ${
-            work.image
-              ? `<img src="${work.image}" alt="${work.title}" class="work-image">`
-              : ""
-          }
-          <div class="work-details">
-            <h3>${work.title}</h3>
-            <p>${work.author}, ${work.type}, ${formattedDate}</p>
+        const date = new Date(work.date);
+        const formattedDate = date.toLocaleDateString("de-DE", {
+          month: "long",
+          year: "numeric",
+        });
+
+        workItem.innerHTML = `
+        <a href="${link}" class="work-link">
+          <div class="work-content">
+            ${
+              work.image
+                ? `<img src="${work.image}" alt="${work.title}" class="work-image">`
+                : ""
+            }
+            <div class="work-details">
+              <h3>${work.title}</h3>
+              <p>${work.author}, ${work.type}, ${formattedDate}</p>
+            </div>
           </div>
-        </div>
+        </a>
       `;
-      worksList.appendChild(workItem);
-    });
+        worksList.appendChild(workItem);
+      });
   } catch (error) {
     console.error("Fehler beim Laden der abgeschlossenen Arbeiten:", error);
   }
